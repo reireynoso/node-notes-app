3040335a1402b9351d853b8224afde45b7f63d05
github username <github email address>
2019-08-01 00:10:45 -0400

    required nodemon

--- app.js
+++ app.js
@@ -5,4 +5,4 @@ const notes = require('./notes')
 console.log(notes())
 
 // console.log(validator.isEmail('example.com'))
-console.log(chalk.green.bold.inverse("Success"))
+console.log(chalk.blue.bold.inverse("Nodemon"))
