2b5ca9bf36f8a51dea321388471fc383a18b042c
github username <github email address>
2019-08-01 21:54:02 -0400

    command line args

--- app.js
+++ app.js
@@ -2,7 +2,15 @@
 const chalk = require('chalk')
 const notes = require('./notes')
 
-console.log(notes())
+// console.log(notes())
 
-// console.log(validator.isEmail('example.com'))
-console.log(chalk.blue.bold.inverse("Nodemon"))
+// // console.log(validator.isEmail('example.com'))
+// console.log(chalk.blue.bold.inverse("Nodemon"))
+
+// console.log(process.argv[2])
+
+const command = process.argv[2]
+
+if(command === 'add'){
+    console.log('Add')
+}
