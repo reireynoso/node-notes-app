cfc16b96596d3fece36dc006789100b2556cc7dc
github username <github email address>
2019-08-01 22:17:28 -0400

    implemented yargs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // const validator = require('validator') //requiring npm 
 const chalk = require('chalk')
+const yargs = require('yargs')
 const notes = require('./notes')
 
 // console.log(notes())
@@ -9,8 +10,59 @@ const notes = require('./notes')
 
 // console.log(process.argv[2])
 
-const command = process.argv[2]
+// const command = process.argv[2]
 
-if(command === 'add'){
-    console.log('Add')
-}
+//add command
+yargs.command({
+    command: "add",
+    describe: "Add a new note",
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv) {
+        // console.log('Adding a new node', argv)
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+//remove command 
+
+yargs.command({
+    command: "remove",
+    describe: "Remove a new note",
+    handler: function() {
+        console.log('Removing a node')
+    }
+})
+
+//list command
+yargs.command({
+    command: "list",
+    describe: "Listing all notes",
+    handler: function() {
+        console.log('List a node')
+    }
+})
+
+//read command
+
+yargs.command({
+    command: "read",
+    describe: "Reading all notes",
+    handler: function() {
+        console.log('Read a node')
+    }
+})
+//add, remove, read, list
+// console.log(yargs.argv)
+yargs.parse()
